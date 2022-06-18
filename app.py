37d47d3ce9ddef14b86d5ab1ca5c5c314daa801f
Queiroz, Jayme <jaymedq@yahoo.com.br>
2022-06-18 18:28:31 -0300

    Update app with login page

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return render_template("index.html")
+    #return "Hello! This is the main page of Central Iot project <h1>Welcome to the home page</h1>"
 
 @app.route("/<name>")
 def user(name:str = "User"):
-    return f"Hello {name}"
+    return render_template("user.html", name = name)
 
 @app.route("/admin")
 def admin():
