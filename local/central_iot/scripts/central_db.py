2e02c37ecc96790a41cf4b911ae4c17f57e7a17c
Jayme <jaymedq@yahoo.com.br>
2022-10-27 01:06:26 -0300

    Update central implementation to use local database and implement script to sync with remote database

--- local/central_iot/scripts/central_db.py
+++ local/central_iot/scripts/central_db.py
@@ -4,7 +4,8 @@ import os
 import sqlite3
 import sqlalchemy
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import MetaData, insert, Table, select, delete, TIMESTAMP, text
+from sqlalchemy import MetaData, select, delete, text, Table, Column
+from sqlalchemy.dialects.sqlite import DATETIME
 from sqlalchemy.engine.url import URL
 
 
@@ -16,15 +17,36 @@ class CentralDb(object):
         self.db_url = db_url
         self.engine = sqlalchemy.create_engine(db_url, echo=True, future=True)
         self.metadata = MetaData()
-        self.metadata.reflect(bind=self.engine)
-        self.sensores_table = self.metadata.tables['sensores']
-        self.dados_table = self.metadata.tables['dados']
+        # self.metadata.reflect(bind=self.engine)
+        # self.sensores_table = self.metadata.tables['sensores']
+        self.sensores_table = Table(
+            "sensores",
+            self.metadata,
+            # additional Column objects which require no change are reflected normally
+            autoload_with=self.engine,
+        )
+        if self.engine.driver == 'pysqlite':
+            self.dados_table = Table(
+                "dados",
+                self.metadata,
+                Column("data_hora", DATETIME(timezone = True, regexp=r"(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)\.?(\d*)")),
+                # additional Column objects which require no change are reflected normally
+                autoload_with=self.engine,
+            )
+        else:
+            self.dados_table = Table(
+                "dados",
+                self.metadata,
+                Column("data_hora", DATETIME(timezone = True)),
+                # additional Column objects which require no change are reflected normally
+                autoload_with=self.engine,
+            )
 
-    def insert_measure(self, device_id, sensor_id, value, unit):
+    def insert_measure(self, id, data_hora: datetime, device_id, sensor_id, value, unit):
         """Inserts a measure in local table"""
         with self.engine.connect() as conn:
             # stt = insert(self.dados_table).values(
-            data_hora=datetime.now().isoformat()
+            data_hora=data_hora.isoformat() or datetime.now().isoformat()
             id_dispositivo=device_id
             id_sensor=sensor_id
             valor=value
@@ -62,8 +84,21 @@ class CentralDb(object):
             stt = delete(self.dados_table)
             conn.execute(stt)
             conn.commit()
-
+    
+    @staticmethod
+    def get_url_from_file(path: os.path = None):
+        with open(path or os.path.join(os.path.dirname(__file__),'central_url.txt')) as file:
+            content = file.read()
+        return content
 
 if __name__ == "__main__":
-    db = CentralDb()
-    print(db.getAllMeasures())
+    remoteDb = CentralDb(CentralDb.get_url_from_file())
+    localDb = CentralDb()
+    allLocalMeasures = []
+    # print(remoteDb.getAllMeasures())
+    allLocalMeasures.extend(localDb.getAllMeasures())
+    if allLocalMeasures:
+        print(allLocalMeasures)
+        for measure in allLocalMeasures:
+            remoteDb.insert_measure(*measure)
+    localDb.deleteAllMeasures()
