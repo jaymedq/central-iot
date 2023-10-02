53ddd3dff60609efea469e69d2451c10fb15249d
Queiroz, Jayme <jaymedq@yahoo.com.br>
2023-10-01 21:33:48 -0300

    Remove URI

--- central_dashboard.py
+++ central_dashboard.py
@@ -19,7 +19,7 @@ colors = {
 }
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
  
-engine = create_engine("postgresql://egpwledbduypwe:04841d07f922d7214941bd6082c7d0336f0ba3245a82c327b6f0dd06d50e1d12@ec2-52-205-98-159.compute-1.amazonaws.com:5432/d8r7ecvlst7083")
+engine = create_engine("")
 df = pd.read_sql_query('select * from "dados"',con=engine)
 # df = pd.read_csv('download.csv')
  
